c3a42948e3489c960dfdf06ef1d3cfbb96c76bff
jesusv0107 <jesus.velasco@rdpolytech.ca>
2024-08-01 13:16:52 -0600

    my first commit

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
 
-const app = express ();
+const app = express();
 const PORT = 3000;
 
-app.get('/users/:id', (req, res) =>{
-    const userId= req.params.id;
-    res.send(`User with ID ${userId}`);
+
+//setting oue view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//route controller with a path parameter
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(User with ID ${userId});
 });
 
 app.get('/', (req, res) =>{
-    res.send('Hello, World!');
+    
+    const data = {
+        title: 'Hello World',
+        message: 'Welcome to my website!'
+    }
+
+    res.render('index', {data});
+
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server started on port ${PORT}');
+
 });
